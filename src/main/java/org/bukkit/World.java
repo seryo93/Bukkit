88930addeeac547f003c6e886a644d5ad70f2919
bloodshot <jdroque@gmail.com>
2013-01-31 00:17:55 -0500

    Added Bukkit environment support for forge dimensions.

--- src/main/java/org/bukkit/World.java
+++ src/main/java/org/bukkit/World.java
@@ -1105,6 +1105,12 @@ public interface World extends PluginMessageRecipient, Metadatable {
             return lookup.get(id);
         }
 
+        // MCPC+ start - allow forge to register environments
+        public static void registerEnvironment(Environment env) {
+            lookup.put(env.getId(),env);
+        }
+        // MCPC+ end
+
         static {
             for (Environment env : values()) {
                 lookup.put(env.getId(), env);
